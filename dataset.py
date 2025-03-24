151f726cdebcff58daf8a2a45159e793bc06df2d
badayvedat <badayvedat@gmail.com>
2025-03-24 10:30:56 +0000

    feat: add wandb logging

--- dataset.py
+++ dataset.py
@@ -1,3 +1,4 @@
+import torch
 import os
 import torchvision.transforms as T
 from torch.utils.data import Dataset
@@ -6,30 +7,29 @@ from torchvision.transforms.functional import to_pil_image
 
 
 class CIFAR10(Dataset):
-    def __init__(self):
-        self.transform = T.Compose([
-            T.RandomHorizontalFlip(),
-            T.ToTensor(),
-            T.Normalize(0.5, 0.5)
-        ])
+    def __init__(self) -> None:
+        self.transform = T.Compose(
+            [T.RandomHorizontalFlip(), T.ToTensor(), T.Normalize(0.5, 0.5)]
+        )
 
-        dataset_dir = 'data/cifar10'
-        self.dataset = datasets.CIFAR10(root=dataset_dir, train=True, download=True, transform=self.transform)
-        
+        dataset_dir = "data/cifar10"
+        self.dataset = datasets.CIFAR10(
+            root=dataset_dir, train=True, download=True, transform=self.transform
+        )
 
-    def __getitem__(self, index):
+    def __getitem__(self, index: int) -> torch.Tensor:
         image, _ = self.dataset[index]
         return image
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.dataset)
 
 
 if __name__ == "__main__":
     dataset = CIFAR10()
-    artifacts_dir = 'artifacts/dataset'
+    artifacts_dir = "artifacts/dataset"
     os.makedirs(artifacts_dir, exist_ok=True)
     for i in range(10):
         image = dataset[i]
         image_pil = to_pil_image(image * 0.5 + 0.5)
-        image_pil.save(os.path.join(artifacts_dir, f'{i}.png'))
+        image_pil.save(os.path.join(artifacts_dir, f"{i}.png"))
--- model.py
+++ model.py
@@ -10,26 +10,26 @@ class TimeEmbedding(nn.Module):
         emb = torch.log(torch.tensor(10_000.0)) / (half_dim - 1)
         self.emb = torch.exp(torch.arange(half_dim, dtype=torch.float32) * -emb)
         self.emb = nn.Parameter(self.emb, requires_grad=False)
-    
-    def forward(self, t: torch.Tensor):
-        pos = t.float().unsqueeze(1)    # [N, 1]
-        emb = pos * self.emb.unsqueeze(0)   # [N, half_dim]
-        emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=1)    # [N, embedding_dim]
+
+    def forward(self, t: torch.Tensor) -> torch.Tensor:
+        pos = t.float().unsqueeze(1)  # [N, 1]
+        emb = pos * self.emb.unsqueeze(0)  # [N, half_dim]
+        emb = torch.cat([torch.sin(emb), torch.cos(emb)], dim=1)  # [N, embedding_dim]
         return emb
 
 
 class TimeDoubleConv(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, time_emb_dim: int):
         super().__init__()
-    
+
         self.conv1 = nn.Conv2d(in_channels, out_channels, 3, padding=1)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.time_mlp = nn.Linear(time_emb_dim, out_channels)
         self.conv2 = nn.Conv2d(out_channels, out_channels, 3, padding=1)
         self.bn2 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
-    
-    def forward(self, x: torch.Tensor, t_emb: torch.Tensor):
+
+    def forward(self, x: torch.Tensor, t_emb: torch.Tensor) -> torch.Tensor:
         x = self.conv1(x)
         x = self.bn1(x)
         t_emb = self.time_mlp(t_emb).unsqueeze(-1).unsqueeze(-1)
@@ -64,28 +64,27 @@ class UNet(nn.Module):
 
         self.out = nn.Conv2d(64, 3, 1)
 
-    
-    def forward(self, x, t):
+    def forward(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
         t_emb = self.time_emb(t).to(x.dtype)
 
-        e1 = self.enc1(x, t_emb)      # (batch_size, 64, 32, 32)
-        p1 = self.pool1(e1)           # (batch_size, 64, 16, 16)
-        e2 = self.enc2(p1, t_emb)     # (batch_size, 128, 16, 16)
-        p2 = self.pool2(e2)           # (batch_size, 128, 8, 8)
-        e3 = self.enc3(p2, t_emb)     # (batch_size, 256, 8, 8)
-        p3 = self.pool3(e3)           # (batch_size, 256, 4, 4)
+        e1 = self.enc1(x, t_emb)  # (batch_size, 64, 32, 32)
+        p1 = self.pool1(e1)  # (batch_size, 64, 16, 16)
+        e2 = self.enc2(p1, t_emb)  # (batch_size, 128, 16, 16)
+        p2 = self.pool2(e2)  # (batch_size, 128, 8, 8)
+        e3 = self.enc3(p2, t_emb)  # (batch_size, 256, 8, 8)
+        p3 = self.pool3(e3)  # (batch_size, 256, 4, 4)
 
         b = self.bottleneck(p3, t_emb)  # (batch_size, 512, 4, 4)
 
-        u3 = self.up3(b)              # (batch_size, 256, 8, 8)
+        u3 = self.up3(b)  # (batch_size, 256, 8, 8)
         cat3 = torch.cat([u3, e3], dim=1)  # (batch_size, 512, 8, 8)
-        d3 = self.dec3(cat3, t_emb)   # (batch_size, 256, 8, 8)
-        u2 = self.up2(d3)             # (batch_size, 128, 16, 16)
+        d3 = self.dec3(cat3, t_emb)  # (batch_size, 256, 8, 8)
+        u2 = self.up2(d3)  # (batch_size, 128, 16, 16)
         cat2 = torch.cat([u2, e2], dim=1)  # (batch_size, 256, 16, 16)
-        d2 = self.dec2(cat2, t_emb)   # (batch_size, 128, 16, 16)
-        u1 = self.up1(d2)             # (batch_size, 64, 32, 32)
+        d2 = self.dec2(cat2, t_emb)  # (batch_size, 128, 16, 16)
+        u1 = self.up1(d2)  # (batch_size, 64, 32, 32)
         cat1 = torch.cat([u1, e1], dim=1)  # (batch_size, 128, 32, 32)
-        d1 = self.dec1(cat1, t_emb)   # (batch_size, 64, 32, 32)
+        d1 = self.dec1(cat1, t_emb)  # (batch_size, 64, 32, 32)
 
-        out = self.out(d1)            # (batch_size, 3, 32, 32)
+        out = self.out(d1)  # (batch_size, 3, 32, 32)
         return out
